ed2adbeea1ffa67a312c444431a4134501addf66
7mood7amed <mm7526347@gmail.com>
2026-07-04 22:42:16 +0300

    Add Render URL to CSRF trusted origins

--- mulkiBH-backend/mulkibh/settings.py
+++ mulkiBH-backend/mulkibh/settings.py
@@ -129,6 +129,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = [
     'https://*.up.railway.app',
     'https://*.vercel.app',
+    'https://*.onrender.com',
     'http://localhost:3000',
     'http://127.0.0.1:8000',
 ]
